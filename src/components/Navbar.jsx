aba6257a28659881b7de7fa2883bbb48df1e56f2
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-03 23:29:08 +0530

    re-learning redux with counter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,12 @@
-import React from 'react'
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 
+export default function Navbar() {
+  const count = useSelector((state) => state.counter.value);
 
-export default function Navbar(props) {
-    
-    const count = useSelector((state) => state.counter.value);
-    const dispatch = useDispatch();
-
-
-    return (
-        <>
-            <h1>I am a navbar and the count is at:{count}</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>I am a navbar and the count is at:{count}</h1>
+    </>
+  );
 }
