d06042af426f37d9be9d06c47789509563600f98
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-16 18:05:29 +0530

    setup done

--- src/redux/counter/counterSlice.js
+++ src/redux/counter/counterSlice.js
@@ -9,10 +9,21 @@ const counterSlice=createSlice({
   name:"counter",
   initialState,
   reducers:{
-    increment:(state)=>{
- state.value+=1
-    }
+    increment:(count)=>{
+ count.value+=1
+    },
+    decrement:(state)=>{
+state.value=-1
+    },
+    multiplyBy2:(state)=>{
+state.value*=2
+    },
+    increseByAmount:(state,action)=>{
+state.value+=action.payload
+    },
   }
 })
+
+
 
 export default counterSlice.reducer
