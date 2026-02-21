0bcbb0fa704939fb8b67c2fb133d07af662df6ab
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-21 18:17:26 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
+import { useState } from "react";
 import "./App.css";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  decrement,
-  increment,
-  multiplyBy2,
-  increaseByAmount,
-} from "./redux/counter/counterSlice";
-
-import Navbar from "./components/Navbar";
 
 function App() {
-  const count = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
 
+  const [count,setCount]=useState("")
   return (
     <div>
-      <Navbar />
-      <h2>{count}</h2>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button
-        disabled={count.value === 0}
-        onClick={() => dispatch(decrement())}
-      >
-        -
-      </button>
-      <button onClick={() => dispatch(multiplyBy2())}>*2</button>
-      <button onClick={() => dispatch(increaseByAmount(4))}>+Amount</button>
+      <h2>Hello</h2>
+
+      <p><span>The counter is at:</span>{count}</p>
+      <button onClick={}>-</button>
+      <button onClick={}>+</button>
     </div>
   );
 }
